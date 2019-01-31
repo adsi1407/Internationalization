7c305ffdccbe87c8c50213ef24a5ab4aaf42615c
David Santacoloma <adsi1407@gmail.com>
2019-01-31 15:44:24 -0500

    Agregar proyecto iOS para implementación internacionalización

---
+++ Internationalization.Touch/MainViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using Internationalization.Core;
+using UIKit;
+
+namespace Internationalization.Touch
+{
+    public class MainViewController: UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.White;
+            Title = "Internationalization";
+            SetupViews();
+        }
+
+        private void SetupViews()
+        {
+            UILabel labelInternationalization = new UILabel();
+            labelInternationalization.Frame = new CGRect(10, 100, 300, 50);
+            labelInternationalization.Text = CommonStrings.Hello;
+            View.AddSubview(labelInternationalization);
+        }
+    }
+}
