069bb2ff65aa23062d6138550592b74873e039ec
David Santacoloma <adsi1407@gmail.com>
2019-01-31 15:27:01 -0500

    Agregar proyecto Android para implementación internacionalización

---
+++ Internationalization.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Internationalization.Core;
+
+namespace Internationalization.Droid
+{
+    [Activity(Label = "Internationalization", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var textViewLanguage = (TextView)FindViewById(Resource.Id.textView_language);
+            textViewLanguage.Text = CommonStrings.Hello;
+        }
+    }
+}
+
